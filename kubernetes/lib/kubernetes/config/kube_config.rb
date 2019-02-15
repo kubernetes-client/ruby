889a8c18346c1dd9a847084a82bab60f97bc3819
Brendan Burns <bburns@microsoft.com>
2019-02-14 21:29:40 -0800

    Fix insecure TLS handling for certs where the host doesn't match the cert.

--- kubernetes/lib/kubernetes/config/kube_config.rb
+++ kubernetes/lib/kubernetes/config/kube_config.rb
@@ -73,7 +73,10 @@ module Kubernetes
           c.base_path = server.path
 
           if server.scheme == 'https'
-            c.verify_ssl = cluster['verify_ssl']
+            if ! cluster['verify-ssl']
+              c.verify_ssl = false
+              c.verify_ssl_host = false
+            end
             c.ssl_ca_cert = cluster['certificate-authority']
             c.cert_file = user['client-certificate']
             c.key_file = user['client-key']
