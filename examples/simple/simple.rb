0fae617b26a21126acc2b7c85ebe5e2cf1e2be57
Brendan Burns <bburns@microsoft.com>
2019-03-07 23:01:16 -0800

    Improve default loading.

--- examples/simple/simple.rb
+++ examples/simple/simple.rb
@@ -1,11 +1,7 @@
 require 'kubernetes'
 require 'pp'
 
-kube_config = Kubernetes::KubeConfig.new("#{ENV['HOME']}/.kube/config")
-config = Kubernetes::Configuration.new()
-
-kube_config.configure(config)
-
+config = Kubernetes::Configuration.default_config()
 client = Kubernetes::CoreV1Api.new(Kubernetes::ApiClient.new(config))
 
 pp client.list_namespaced_pod('default')
