43885e7be545e2ba92dc379c5c12dff58cdc4472
Brendan Burns <bburns@microsoft.com>
2019-03-12 18:30:02 -0700

    Add style linting with rubocop

--- kubernetes/src/kubernetes/utils.rb
+++ kubernetes/src/kubernetes/utils.rb
@@ -15,8 +15,8 @@
 require 'kubernetes/config/incluster_config'
 require 'kubernetes/config/kube_config'
 
+# The Kubernetes module encapsulates the Kubernetes client for Ruby
 module Kubernetes
-
   #
   # Use the service account kubernetes gives to pods to connect to kubernetes
   # cluster. It's intended for clients that expect to be running inside a pod
@@ -31,10 +31,12 @@ module Kubernetes
   # Loads authentication and cluster information from kube-config file
   # and stores them in Kubernetes::Configuration.
   # @param config_file [String] Path of the kube-config file.
-  # @param context [String] Set the active context. If is set to nil, current_context from config file will be used.
-  # @param client_configuration [Kubernetes::Configuration] The Kubernetes::Configuration tp set configs to.
+  # @param context [String] Set the active context. If is set to nil,
+  #   current_context from config file will be used.
+  # @param client_configuration [Kubernetes::Configuration] The
+  #   Kubernetes::Configuration tp set configs to.
   def load_kube_config(
-    config_file=ENV['KUBECONFIG'],
+    config_file = ENV['KUBECONFIG'],
     context: nil,
     client_configuration: Configuration.default
   )
@@ -48,17 +50,41 @@ module Kubernetes
   # to be used with any API object. This will allow the caller to concurrently
   # talk with multiple clusters.
   # @param config_file [String] Path of the kube-config file.
-  # @param context [String] Set the active context. If is set to nil, current_context from config file will be used.
+  # @param context [String] Set the active context. If is set to nil,
+  #    current_context from config file will be used.
   # @return [Kubernetes::ApiClient] Api client for Kubernetes cluster
   def new_client_from_config(
-    config_file=ENV['KUBECONFIG'],
+    config_file = ENV['KUBECONFIG'],
     context: nil
   )
     config_file ||= KubeConfig::KUBE_CONFIG_DEFAULT_LOCATION
     client_configuration = Configuration.new
-    load_kube_config(config_file, context: context, client_configuration: client_configuration)
+    load_kube_config(config_file,
+                     context: context,
+                     client_configuration: client_configuration)
     ApiClient.new(client_configuration)
   end
 
-  extend self
+  def create_temp_file_and_set(obj, key)
+    return unless obj && !obj[key] && obj["#{key}-data"]
+
+    obj[key] = create_temp_file_with_base64content(obj["#{key}-data"])
+  end
+
+  @temp_files = {}
+
+  def create_temp_file_with_base64content(content)
+    @temp_files[content] ||= Tempfile.open('kube') do |temp|
+      temp.write(Base64.strict_decode64(content))
+      temp.path
+    end
+  end
+
+  def cache_temp_file(content, path)
+    @temp_files[content] = path
+  end
+
+  module_function :new_client_from_config, :load_incluster_config,
+                  :load_kube_config, :create_temp_file_and_set,
+                  :create_temp_file_with_base64content, :cache_temp_file
 end
