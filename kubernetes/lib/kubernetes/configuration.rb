fc2e8ad674b29abb240ef828f2ca472f3bf1f293
Jeroen Nijhof <jeroen@jeroennijhof.nl>
2023-08-25 12:32:12 +0200

    URI encode is obsolete

--- kubernetes/lib/kubernetes/configuration.rb
+++ kubernetes/lib/kubernetes/configuration.rb
@@ -175,7 +175,7 @@ module Kubernetes
 
     def base_url
       url = "#{scheme}://#{[host, base_path].join('/').gsub(/\/+/, '/')}".sub(/\/+\z/, '')
-      URI.encode(url)
+      URI(url)
     end
 
     # Gets API key (with prefix if set).
